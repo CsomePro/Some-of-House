d8624ea18de5da1f086c4a0b2d046701105e94cb
Csomepro <csomee@qq.com>
2024-11-11 21:37:02 +0800

    add: enhance debuginfo script with verbose option and improve error handling

--- debuginfo.sh
+++ debuginfo.sh
@@ -1,28 +1,67 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <binary>"
+function usage() {
+    echo "Usage: $0 [-v] <binary>"
+    echo "Options:"
+    echo "  -v: debuginfod-find verbose mode"
     exit 1
+}
+
+verbose=0
+while getopts ":v" opt; do
+    case ${opt} in
+        v )
+            verbose=1
+            shift $((OPTIND -1))
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" 1>&2
+            usage
+            ;;
+    esac
+done
+
+if [ $# -ne 1 ]; then
+    usage
 fi
+
+binary=$1
 
 # 1. get debuginfo from debuginfod 
 # 2. copy debuginfo file to /usr/lib/debug/.build-id/
 
 # get file buildID
-buildID=$(eu-readelf -n $1 | grep "Build ID" | awk '{print $3}')
+buildID=$(eu-readelf -n $binary | grep "Build ID" | awk '{print $3}')
 
 # export DEBUGINFOD_URLS="https://debuginfod.elfutils.org/"
 # download debuginfo file
-debug_file=$(DEBUGINFOD_URLS="https://debuginfod.elfutils.org/" debuginfod-find debuginfo $buildID)
+debug_flag=""
+if [ $verbose -eq 1 ]; then
+    debug_flag="-v"
+fi
 
-echo "debug_file: $debug_file"
+debug_file=$(DEBUGINFOD_URLS="https://debuginfod.elfutils.org/" debuginfod-find $debug_flag debuginfo $buildID)
+
+if [ $? -ne 0 ]; then
+    echo "Failed to get debuginfo file."
+    exit 1
+fi
+
+if [ -z "$debug_file" ]; then
+    echo "Failed to get debuginfo file."
+    exit 1
+fi
+
+echo ""
+echo -e "\e[32mSuccessfully get debuginfo file.\e[0m"
+echo "Debug file: $debug_file"
 
 build_id_prefix_2=${buildID:0:2}
 build_id_suffix=${buildID:2}
 
 target_file=/usr/lib/debug/.build-id/$build_id_prefix_2/$build_id_suffix.debug
 
-echo "target_file: $target_file"
+echo "Copy to $target_file"
 
 cp $debug_file $target_file
 if [ $? -ne 0 ]; then
@@ -33,6 +72,13 @@ if [ $? -ne 0 ]; then
     else
         echo "Operation aborted."
     fi
-else
+fi
+
+# Check if the file permissions are 766, if not, change them
+current_permissions=$(stat -c "%a" $target_file)
+if [ "$current_permissions" -ne 766 ]; then
+    # echo "Changing permissions of $target_file to 766"
     chmod 766 $target_file
 fi
+
+echo -e "\e[32mDone.\e[0m"
